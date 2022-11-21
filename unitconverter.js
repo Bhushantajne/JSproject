62c0c840cf87cf12a7a14bde8c014d90f17b60d4
Bhushan tajne <you@example.com>
2022-11-21 04:13:40 -0400

    first project

---
+++ unitconverter.js
@@ -0,0 +1,23 @@
+var feet = document.getElementById('feet');
+
+var inche = document.getElementById('inches');
+
+
+feet.addEventListener('input',function()
+{
+    let f=this.value;
+    let i=f*12;
+    inche.value=i;
+});
+
+inche.addEventListener('input',function()
+{
+    let i=this.value;
+    let f=i/12;
+
+     if(!Number.isInteger(f)){
+        f=f.toFixed(2)
+     }
+
+    feet.value=f;
+});
